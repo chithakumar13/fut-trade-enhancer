c94776bcd9820d2fb5ccb4fa42bca2e156941659
Ck Algos <chithakumar13@gmail.com>
2021-08-19 01:07:59 +0530

    Version Update

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,7 +2,7 @@ module.exports = {
   headers: {
     name: "FUT Trade Enhancer",
     namespace: "http://tampermonkey.net/",
-    version: "1.0.0",
+    version: "1.0.1",
     description: "FUT Trade Enhancer",
     author: "CK Algos",
     match: [
